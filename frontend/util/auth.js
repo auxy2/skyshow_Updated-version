8455eac32909a2a45f0c5fce79505b5dff9d1116
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-09-30 18:34:13 +0100

    initial commit

---
+++ frontend/util/auth.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+// const loginAndSIgnUp = async (mode, data) => {
+//   const response = await axios.post(
+//     `https://1fe3-102-88-63-207.ngrok-free.app/api/V1/skyshowNG/${mode}`,
+//     data
+//   );
+//   return response.data;
+// };
+const url = `https://d50c-197-210-28-193.ngrok-free.app`;
+
+export async function createUser(payload) {
+  const response = await axios.post(`${url}/api/V1/skyshowNG/signUp`, payload);
+  console.log(response.data, "response");
+  return response.data;
+}
+
+export async function verifyEmailAndNumber(option, data) {
+  if (option === "Email") {
+    const response = await axios.post(
+      `${url}/api/V1/skyshowNG/signUp?email=${data}`
+    );
+    return response.data;
+  }
+
+  if (option === "Number") {
+    const response = await axios.post(
+      `${url}/api/V1/skyshowNG/signUp?Number=${data}`
+    );
+    return response.data;
+  }
+}
+
+export async function login(payload) {
+  const response = await axios.post(`${url}/api/V1/skyshowNG/login`, payload);
+
+  return response.data;
+}
+
+export async function addBank() {
+  const response = axios.get(`${url}/api/V1/skyshowNG/addBank?acc`);
+}
